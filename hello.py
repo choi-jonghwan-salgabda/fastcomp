4bbf37486ef6e5d5fadcafe4b8e6fb4853584597
salgabda <salgabda@naver.com>
2024-10-24 15:45:17 +0900

    refeator: Return Complete String instead of Print_hello

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 # 반갑습니다. 첫번째 고침입니다.
 
-def print_hello(value: str ) -> None:
-    return print(f'반가워요, {value}')
+def print_hello(value: str ) -> Str:
+    if isinstance(value, str):
+        return f'반가워요, {value}'
+    else:
+        return "This id not string"
 
 
 if __name__=='__main__':
